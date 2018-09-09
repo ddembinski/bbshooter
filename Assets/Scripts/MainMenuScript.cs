90453216827cedfee2e2d119437b13bad780b4ec
ddembinski <dembinski.david@gmail.com>
2018-09-08 22:24:38 -0400

    we got a working volume slider, boys

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+	public void SetVolume(float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetSliderPosition() {
+        float currentVolume;
+        audioMixer.GetFloat("volume", out currentVolume);
+        volumeSlider.GetComponent<Slider>().value = currentVolume;
+    }
+
+    void Awake() {
+        SetSliderPosition();
+    }
+}
